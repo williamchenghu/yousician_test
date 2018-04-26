0dcfd78374ecc48fcf198d141a588363744e2ec3
Zhen Shi <zoe.zhenshi@gmail.com>
2018-04-26 13:03:59 +0300

    - search bar style - filter bar style

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './style/index.css'
-import SongsListContainer from './containers/SongsListContainer'
+import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 import axios from 'axios'
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 axios.defaults.baseURL = 'http://localhost:3001/api/'
 
+const muiTheme = getMuiTheme({
+  slider: {
+    selectionColor: '#4fc514',
+    handleFillColor: '#4fc514',
+  },
+})
+
 ReactDOM.render(
-  <MuiThemeProvider>
-    <SongsListContainer />
+  <MuiThemeProvider muiTheme={muiTheme}>
+    <App />
   </MuiThemeProvider>,
   document.getElementById('root')
 )
